ad22f0fc83dc1c1619f8250d6da27569dcc29a55
Mette Munne <mette.ruusu@gmail.com>
2023-12-05 20:02:03 +0200

    Fix card class tests

--- src/tests/sprites/card_test.py
+++ src/tests/sprites/card_test.py
@@ -20,20 +20,36 @@ class TestCard(unittest.TestCase):
         self.card.flip()
         self.assertTrue(self.card.is_open)
 
-    def test_card_not_open_and_mouse_over_before_choosing(self):
+    def test_flip_from_not_open_to_open(self):
         self.card.is_open = False
-        mouse_position = (10, 10)
-        self.card.card_chosen(mouse_position)
+        self.card.flip()
         self.assertTrue(self.card.is_open)
 
-    def test_card_not_open_and_mouse_not_over_before_choosing(self):
-        self.card.is_open = False
-        mouse_position = (100, 100)
-        self.card.card_chosen(mouse_position)
+    def test_flip_from_open_to_not_open(self):
+        self.card.is_open = True
+        self.card.flip()
         self.assertFalse(self.card.is_open)
 
-    def test_card_opened_and_mouse_not_over_before_choosing(self):
-        self.card.is_open = True
-        mouse_position = (100, 100)
-        self.card.card_chosen(mouse_position)
-        self.assertTrue(self.card.is_open)
+    def test_mouse_over_(self):
+        mouse_position_x = 10
+        mouse_position_y = 10
+        result = self.card.card_chosen(mouse_position_x, mouse_position_y)
+        self.assertTrue(result)
+
+    def test_mouse_not_over(self):
+        mouse_position_x = 100
+        mouse_position_y = 100
+        result = self.card.card_chosen(mouse_position_x, mouse_position_y)
+        self.assertFalse(result)
+
+    def test_cards_match(self):
+        card1 = self.card
+        card2 = self.card
+        result = card1.is_matching(card2)
+        self.assertTrue(result)
+    
+    def test_cards_not_matching(self):
+        card1 = self.card
+        card2 = Card(back_image_path, image_path, 0, 0)
+        result = card1.is_matching(card2)
+        self.assertFalse(result)
